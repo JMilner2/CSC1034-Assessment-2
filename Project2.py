ccab02240f219b2d04b5267bb16dc36e08bede23
joshu <j.milner2@newcastle.ac.uk>
2021-11-11 15:59:02 +0000

    Created contacts class and added contacts text file to store info

--- Project2.py
+++ Project2.py
@@ -6,5 +6,25 @@ class Contact:
         self.birthdate = birthdate
 
     def displayinfo(self):
-        return f"{self.name} address:{self.address}, phone number:{self.number}, birthdate:{self.birthdate}"
-    
+        return f"{self.name}:" \
+               f" address={self.address}, phone number={self.number}, birthdate={self.birthdate}"
+
+
+def get_contact_details():
+    f = open("Contacts.txt","r")
+    contacts = f.readlines()
+    f.close()
+    return contacts
+
+
+def create_contact(contacts,i):
+    contact = contacts[i].split(", ")
+    c = Contact(contact[0],contact[1],contact[2],contact[3])
+    return c
+
+
+
+contactslist = get_contact_details()
+contacts_dic = {"c"+str(x):create_contact(contactslist,x) for x in range(len(contactslist))}
+print(contacts_dic["c1"].displayinfo())
+
