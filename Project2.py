cb742afc09d2fa9a5b87fd0b45b68452a0d82394
JMilner2 <j.milner2@newcastle.ac.uk>
2021-11-17 20:41:01 +0000

    fixed bug where you couldnt search for contact by DOB

--- Project2.py
+++ Project2.py
@@ -20,22 +20,32 @@ class Contact:
             if self.number == contact_detail:
                 print(self.displayinfo())
         elif number == "4":
-            if self.birthdate == contact_detail:
+            if self.birthdate == (contact_detail + "\n"):
                 print(self.displayinfo())
         else:
             print("INVALID CHOICE MADE")
 
-    def change_contact_details(self):  # NEEDS VALIDATION AND ERROR CHECKING
-        self.name = input("ENTER A NAME FOR CONTACT: ")
-        self.address = input("ENTER AN ADDRESS FOR CONTACT: ")
-        self.number = input("ENTER A PHONE NUMBER FOR CONTACT: ")
-        self.birthdate = input("ENTER A DATE OF BIRTH FOR CONTACT(XX/XX/XXXX): ")
+    def change_contact_details(self):
+        self.name = check_blank(input("ENTER A NAME FOR CONTACT: "))
+        self.address = check_blank(input("ENTER AN ADDRESS FOR CONTACT: "))
+        self.number = check_blank(input("ENTER A PHONE NUMBER FOR CONTACT: "))
+        self.birthdate = check_blank(input("ENTER A DATE OF BIRTH FOR CONTACT(XX/XX/XXXX): "))
 
     def save_contact(self):
-        file = open("Contacts.txt","a")
+        file = open("Contacts.txt", "a")
         contact_details = (self.name + ", " + self.address + ", " + self.number + ", " + self.birthdate + "\n")
         file.write(contact_details)
         file.close()
+
+
+def check_blank(uinput):
+    while True:
+        if uinput == "" or uinput == " ":
+            uinput = input("INPUT CANT BE BLANK\n"
+                           "ENTER: ")
+        else:
+            break
+    return uinput
 
 
 def get_contact_details():
@@ -58,7 +68,7 @@ def edit_contact(contacts_list):
     print("WHICH CONTACT WOULD YOU LIKE TO EDIT:")
     for i in contacts_list:
         print(i + " = " + contacts_list[i].displayinfo())
-    contact_to_edit = input("PLEASE CHOOSE A CONTACT TO EDIT(E.G. c1,c2)\n"  # NEEDS VALIDATION AND ERROR CHECKING
+    contact_to_edit = input("PLEASE CHOOSE A CONTACT TO EDIT(E.G. c1,c2)\n" 
                             "CHOICE: ")
     try:
         contacts_list[contact_to_edit].change_contact_details()
@@ -68,11 +78,11 @@ def edit_contact(contacts_list):
 
 
 def add_new_contact():
-    name = input("New contacts name: ")  # NEEDS VALIDATION AND ERROR CHECKING
-    address = input("New contacts address: ")
-    number = input("New contacts number: ")
-    dob = input("New contacts date of birth(xx/xx/xxxx): ")
-    new_contact = Contact(name,address,number,dob)
+    name = check_blank(input("New contacts name: "))
+    address = check_blank(input("New contacts address: "))
+    number = check_blank(input("New contacts number: "))
+    dob = check_blank(input("New contacts date of birth(xx/xx/xxxx): "))
+    new_contact = Contact(name, address, number, dob)
     new_contact.save_contact()
 
 
@@ -91,28 +101,36 @@ while True:
     user_input = False
     while not user_input:
         try:
-            user_choice = input("ENTER WHAT YOU WANT YOU DO: \n"
+            user_choice = input("\nENTER WHAT YOU WANT YOU DO: \n"
                                 "1 = search for contact \n"
                                 "2 = print the whole list of contacts \n"
                                 "3 = edit a contacts details \n"
                                 "4 = add a new contact \n"
                                 "5 = save and exit contacts \n"
-                                "CHOICE = ")
+                                "\nCHOICE = ")
             float(user_choice)
             user_input = True
         except ValueError:
             print("ONLY ENTER THE NUMBER FOR WHAT YOU WANT TO DO")
 
     if user_choice == "1":
-        choice = input("WHAT DO YOU WANT TO SEARCH BY?:\n"  # NEEDS VALIDATION CHECKS
-                       "1 = NAME\n"
-                       "2= ADDRESS\n"
-                       "3= PHONE NUMBER\n"
-                       "4 = DOB\n"
-                       "CHOICE = ")
-        userinfo = input("ENTER : ")
+        choice = ""
+        while True:
+            validchoice = ["1", "2", "3", "4"]
+            if choice not in validchoice:
+                print("\nCHOOSE FROM THE LIST FROM BELOW")
+                choice = input("WHAT DO YOU WANT TO SEARCH BY?:\n"  
+                               "1 = NAME\n"
+                               "2= ADDRESS\n"
+                               "3= PHONE NUMBER\n"
+                               "4 = DOB\n"
+                               "\nCHOICE = ")
+            else:
+                break
+        userinfo = check_blank(input("ENTER : "))
         for i in contacts_dic:
             contacts_dic[i].search_contact(userinfo, choice)
+        print("\nALL CONTACTS HAVE BEEN SEARCHED FOR MATCHING DETAILS\n")
 
     elif user_choice == "2":
         for i in contacts_dic:
@@ -133,4 +151,3 @@ while True:
     else:
         print("INVALID CHOICE")
     update_contacts_list(contacts_dic)
-
